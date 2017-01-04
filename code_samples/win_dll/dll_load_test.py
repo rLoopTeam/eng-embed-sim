8db858a523206b775c68967f8a79becdcd31c80a
Ryan Adams <radams@cyandata.com>
2017-01-03 23:52:58 -0800

    Milestone: Working callback! (vDEBUG_PRINTF_WIN32__Set_Callback)

--- code_samples/win_dll/dll_load_test.py
+++ code_samples/win_dll/dll_load_test.py
@@ -14,7 +14,7 @@ dll_path = "C:\\Users\\radams\\Documents\\rLoop\\code\\eng-software-pod\\APPLICA
 dll_name = "LDLL173__RLOOP__LCCM653.dll"
 dll_filename = os.path.join(dll_path, dll_name)
 print "DLL {} exists? {}".format(dll_filename, os.path.exists(dll_filename))
-lib = ctypes.WinDLL(dll_filename)
+lib = ctypes.CDLL(dll_filename)
 print "Lib loaded successfully!"
 value = lib.vPWRNODE__Init()
 print "Called vPWRNODE__Init()! Got {} in return.".format(value)
@@ -23,3 +23,46 @@ try:
     print "Called not_a_real_function(), got {} in return.".format(hmm)
 except Exception as e:
     print "Called a fake function, got an (expected) exception! ({})".format(e)
+    
+print "-----"
+print "Now let's try a callback..."
+# @see http://stackoverflow.com/questions/17980167/writing-python-ctypes-for-function-pointer-callback-function-in-c
+
+#debug_printf_callback = ctypes.WINFUNCTYPE(None, ctypes.POINTER(ctypes.c_byte))  # Returns nothing, takes a byte*
+
+debug_printf_callback = ctypes.WINFUNCTYPE(None, ctypes.c_char_p)  # Returns nothing, takes a byte*
+
+vDEBUG_PRINTF_WIN32__Set_Callback = lib.vDEBUG_PRINTF_WIN32__Set_Callback
+vDEBUG_PRINTF_WIN32__Set_Callback.argtypes = [debug_printf_callback]
+vDEBUG_PRINTF_WIN32__Set_Callback.restype = None
+
+def debug_printf(val):
+    print "Python Debug printf callback called with value '{}'".format(val)
+    
+# reference the callback to keep it alive
+_debug_printf_callback = debug_printf_callback(debug_printf)
+
+vDEBUG_PRINTF_WIN32__Set_Callback(_debug_printf_callback)
+
+from time import sleep
+
+print "Calling vPWRNODE__Init()"
+def pwrnode_init():
+    lib.vPWRNODE__Init()
+
+def pwrnode_process():
+    lib.vPWRNODE__Process()
+
+pwrnode_init()
+n = 100
+print "Starting vPWRNODE__Process() loop ({})".format(n)
+for i in xrange(n):
+    print "  Calling vPWRNODE__Process()..."
+    #lib.vPWRNODE__Process()
+    pwrnode_process()
+    print "After vPWRNODE__Process()"
+    
+print "Done with vPWRNODE__Process() loop."
+
+# @todo: The program exits during the lib.vPWRNODE__Process() call -- maybe need to initialize temperature or something? 
+
