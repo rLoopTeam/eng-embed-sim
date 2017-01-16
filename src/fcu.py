490f2f846427c17d65fe33f48f6059c04a1f3a39
Ryan Adams <radams@cyandata.com>
2017-01-15 23:36:10 -0800

    FCU callbacks set up; ready to test. FCU file documented with functions

--- src/fcu.py
+++ src/fcu.py
@@ -51,39 +51,123 @@ class Fcu:
         # The debugger callback
         #Public Delegate Sub DEBUG_PRINTF__CallbackDelegate(ByVal pu8String As IntPtr)
         #Private Shared Sub vDEBUG_PRINTF_WIN32__Set_Callback(ByVal callback As MulticastDelegate)
-        self.register_callback(self.debug_printf_callback, "vDEBUG_PRINTF_WIN32__Set_Callback", None, [ctypes.c_char_p])
+        self.register_callback(self.debug_printf_callback, 
+            'vDEBUG_PRINTF_WIN32__Set_Callback', None, 
+            [ctypes.c_char_p])
         
-        # Stepdrive Update Position Callback
-        # Public Shared Sub vSTEPDRIVE_WIN32__Set_UpdatePositionCallback(ByVal callback As MulticastDelegate)
-        # Public Delegate Sub STEPDRIVE_WIN32__Set_UpdatePositionCallbackDelegate(u8MotorIndex As Byte, u8Step As Byte, u8Dir As Byte, s32Position As Int32)
-        self.register_callback(self.stepdrive_update_position_callback, 'vSTEPDRIVE_WIN32__Set_UpdatePositionCallback', None, [ctypes.c_ubyte, ctypes.c_ubyte, ctypes.c_ubyte, ctypes.c_int32])
-
+        # 'Ethernet
         # Public Shared Sub vETH_WIN32__Set_Ethernet_TxCallback(ByVal callback As MulticastDelegate)
         # Public Delegate Sub ETH_WIN32__TxCallbackDelegate(ByVal pu8Buffer As IntPtr, ByVal u16BufferLength As UInt16)
-        self.register_callback(self.eth_tx_callback, 'vETH_WIN32__Set_Ethernet_TxCallback', None, [ctypes.c_byte_p, ctypes.c_uint16])
+        self.register_callback(self.eth_tx_callback, 
+            'vETH_WIN32__Set_Ethernet_TxCallback', None, 
+            [ctypes.POINTER(ctypes.c_ubyte), ctypes.c_uint16])
 
+        # 'mma8451
+        # Public Shared Sub vMMA8451_WIN32__Set_ReadDataCallback(ByVal callback As MulticastDelegate)
+        # Public Delegate Sub MMA8451_WIN32__ReadDataCallbackDelegate(u8DeviceIndex As Byte, pu8X As IntPtr, pu8Y As IntPtr, pu8Z As IntPtr)
+        self.register_callback(self.MMA8451_readdata_callback, 
+            'vMMA8451_WIN32__Set_ReadDataCallback', None, 
+            [ctypes.c_ubyte, ctypes.POINTER(ctypes.c_uint8), ctypes.POINTER(ctypes.c_uint8), ctypes.POINTER(ctypes.c_uint8)])
+        
+        # 'stepper system
+        # Public Shared Sub vSTEPDRIVE_WIN32__Set_UpdatePositionCallback(ByVal callback As MulticastDelegate)
+        # Public Delegate Sub STEPDRIVE_WIN32__Set_UpdatePositionCallbackDelegate(u8MotorIndex As Byte, u8Step As Byte, u8Dir As Byte, s32Position As Int32)
+        self.register_callback(self.stepdrive_update_position_callback, 
+            'vSTEPDRIVE_WIN32__Set_UpdatePositionCallback', None, 
+            [ctypes.c_ubyte, ctypes.c_ubyte, ctypes.c_ubyte, ctypes.c_int32])
+
+        # 'SC16 UARTS
+        # Public Shared Sub vSC16IS_WIN32__Set_TxData_Callback(u8DeviceIndex As Byte, ByVal callback As MulticastDelegate)
+        # Public Delegate Sub SC16IS_WIN32__Set_TxData_CallbackDelegate(u8DeviceIndex As Byte, pu8Data As IntPtr, u8Length As Byte)
+        self.register_callback(self.SC16IS_txdata_callback, 
+            'vSC16IS_WIN32__Set_TxData_Callback', None,
+            [ctypes.c_uint8, ctypes.POINTER(c_uint8), ctypes.c_uint8])
+        
+        # 'AMC7812 for HE Thrott
+        # Public Delegate Sub AMC7812_WIN32__Set_DACVoltsCallbackDelegate(u8Channel As Byte, f32Volts As Single)
+        # Public Shared Sub vAMC7812_WIN32__Set_DACVoltsCallback(ByVal callback As MulticastDelegate)
+        self.register_callback(self.AMC7812_DAC_volts_callback, 
+            'AMC7812_WIN32__Set_DACVoltsCallbackDelegate', None,
+            [ctypes.c_uint8, ctypes.c_float32])
         
         # ------------------------
         #  Callable Functions
         # ------------------------
 
-        # Public Shared Sub vETH_WIN32__Ethernet_Input(pu8Buffer() As Byte, u16BufferLength As UInt16)
+        # '''Debugging / Testing / Simulating
         
+        # Public Shared Sub vETH_WIN32__Ethernet_Input(pu8Buffer() As Byte, u16BufferLength As UInt16)
+        # Public Shared Sub vMMA8451_WIN32__TriggerInterrupt(u8DeviceIndex As Byte)
+        # Public Shared Sub vSTEPDRIVE_WIN32__ForcePosition(u8MotorIndex As Byte, s32Position As Int32)
+        # Public Shared Sub vSC16IS_WIN32__InjectData(u8DeviceIndex As Byte, pu8Data() As Byte, u8Length As Byte)
+
+        # '''Running
+        
+        # 'Control and Timing
+        # Private Shared Sub vFCU__Init()
+        # Private Shared Sub vFCU__Process()
+        # Private Shared Sub vFCU__RTI_10MS_ISR()
+        # Private Shared Sub vFCU__RTI_100MS_ISR()
+        # Private Shared Sub vSTEPDRIVE_TIMEBASE__ISR()   # Note: 50usec
+        
+        # 'Laser Distance
+        # Private Shared Sub vFCU_LASERDIST_WIN32__Set_DistanceRaw(f32Value As Single)
+
+        # 'laser optoncdt
+        # Private Shared Sub vFCU_LASEROPTO_WIN32__Set_DistanceRaw(u32Index As UInt32, f32Value As Single)
+        
+        # 'brake switches
+        # Private Shared Sub vFCU_BRAKES_SW_WIN32__Inject_SwitchState(u8Brake As Byte, u8ExtendRetract As Byte, u8Value As Byte)
+        # Private Shared Sub vFCU_BRAKES_SW__Left_SwitchExtend_ISR()
+        # Private Shared Sub vFCU_BRAKES_SW__Left_SwitchRetract_ISR()
+        # Private Shared Sub vFCU_BRAKES_SW__Right_SwitchExtend_ISR()
+        # Private Shared Sub vFCU_BRAKES_SW__Right_SwitchRetract_ISR()
+        
+        # 'MLP
+        # Private Shared Sub vFCU_BRAKES_MLP_WIN32__ForceADC(u8Brake As Byte, u16Value As UInt16)
+
+        # 'ASI
+        # Private Shared Function u8FCU_ASI_MUX_WIN32__Get() As Byte
+        # Private Shared Function u16FCU_ASI_CRC__ComputeCRC(pu8Data() As Byte, u16DataLen As UInt16) As UInt16
+
+        # 'Testing Area
+        # Private Shared Sub vLCCM655R0_TS_000()
+        # Private Shared Sub vLCCM655R0_TS_001()
+        # Private Shared Sub vLCCM655R0_TS_002()
+        # Private Shared Sub vLCCM655R0_TS_003()
+        # Private Shared Sub vLCCM655R0_TS_004()
+        # Private Shared Sub vLCCM655R0_TS_005()
+        # Private Shared Sub vLCCM655R0_TS_006()
+
+
         
     def debug_printf_callback(self, message):
         # Public Delegate Sub DEBUG_PRINTF__CallbackDelegate(ByVal pu8String As IntPtr)
-        self.logger.debug("FCU.debug_printf('{}')".format(message))
-    
-    def stepdrive_update_position_callback(self, u8MotorIndex, u8Step, u8Dir, s32Position):
-        # Public Delegate Sub STEPDRIVE_WIN32__Set_UpdatePositionCallbackDelegate(u8MotorIndex As Byte, u8Step As Byte, u8Dir As Byte, s32Position As Int32)
-        self.logger.debug("FCU.stepdrive_update_position_callback({}, {}, {}, {})".format(u8MotorIndex, u8Step, u8Dir, s32Position))
-        
+        self.logger.debug("Fcu.debug_printf('{}')".format(message))
+
     def eth_tx_callback(self, pu8Buffer, u16BufferLength):
         # Public Delegate Sub ETH_WIN32__TxCallbackDelegate(ByVal pu8Buffer As IntPtr, ByVal u16BufferLength As UInt16)
         # @todo: Format the buffer so it's readable (bytes)
-        self.logger.debug("FCU.eth_tx_callback('{}', {})".format(pu8Buffer, u16BufferLength)))
-         
+        self.logger.debug("Fcu.eth_tx_callback('{}', {})".format(pu8Buffer, u16BufferLength))
+
+    def MMA8451_readdata_callback(self, u8DeviceIndex, pu8X, pu8Y, pu8Z):
+        # Public Delegate Sub MMA8451_WIN32__ReadDataCallbackDelegate(u8DeviceIndex As Byte, pu8X As IntPtr, pu8Y As IntPtr, pu8Z As IntPtr)
+        self.logger.debug("Fcu.MMA8451_readdata_callback({}, {}, {}, {})".format(u8DeviceIndex, pu8X, pu8Y, pu8Z))
+
+    def stepdrive_update_position_callback(self, u8MotorIndex, u8Step, u8Dir, s32Position):
+        # Public Delegate Sub STEPDRIVE_WIN32__Set_UpdatePositionCallbackDelegate(u8MotorIndex As Byte, u8Step As Byte, u8Dir As Byte, s32Position As Int32)
+        self.logger.debug("Fcu.stepdrive_update_position_callback({}, {}, {}, {})".format(u8MotorIndex, u8Step, u8Dir, s32Position))
+
+    def SC16IS_txdata_callback(self, u8DeviceIndex, pu8Data, u8Length):
+        # Public Delegate Sub SC16IS_WIN32__Set_TxData_CallbackDelegate(u8DeviceIndex As Byte, pu8Data As IntPtr, u8Length As Byte)
+        self.logger.debug("Fcu.SC16IS_txdata_callback({}, {}, {})".format(u8DeviceIndex, pu8Data, u8Length))
+
+    def AMC7812_DAC_volts_callback(self, u8Channel, f32Volts):
+        # Public Delegate Sub AMC7812_WIN32__Set_DACVoltsCallbackDelegate(u8Channel As Byte, f32Volts As Single)
+        self.logger.debug("Fcu.AMC7812_DAC_volts_callback({}, {})".format(u8Channel, f32Volts))
+
     def register_callback(self, python_function, dll_function_name, restype, args):
+
         # Create the callback functype
         callback_functype = ctypes.CFUNCTYPE(restype, *args)
 
