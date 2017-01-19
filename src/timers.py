aa85615043372c5c20c334ba13d953f452792035
Ryan Adams <radams@cyandata.com>
2017-01-18 22:33:45 -0800

    Working on time dialation

--- src/timers.py
+++ src/timers.py
@@ -8,19 +8,29 @@
 import time
 from datetime import datetime
 import threading
-
 import numpy as np
 
-class TimerMs:
-    def __init__(self, interval_ms, callback):
-        self.interval_ms = interval_ms
+from units import Units
+
+class Timer:
+    """ Basic timer with callback and time dialation """
+
+    def __init__(self, interval_sec, callback):
+        self.interval = interval_sec
         self.callback = callback
-        
-        self.delay = self.interval_ms / 1000.0
-        
-        # Note: no need to calibrate, ms timing is pretty accurate without it.
-        
+
+
+        # Time dialation factor (for adjusting real time to sim time for timers)
+        self.dialation = 1.0
+        self.delay = self.interval  # Will be adjusted by the set_dialation method
+        self.set_dialation(self.dialation)
+
         self.stop_flag = False
+
+    def set_dialation(self, dialation):
+        """ set the time-dialated delay """
+        self.dialation = dialation
+        self.delay = self.interval * self.dialation
 
     def start_threaded(self):
         t = threading.Thread(target=self.run, args=())
@@ -41,150 +51,60 @@ class TimerMs:
 
     def stop(self):
         self.stop_flag = True
-
-
-class TimerUsec: 
-    def __init__(self, interval_usec, callback):
-        self.interval_usec = interval_usec
-        self.callback = callback
-
-        self.requested_delay = interval_usec / 1000000.0
-        #self.overhead = 0.0000005  # This is a decent guess
-        self.overhead = 0.0
-        self.delay = self.requested_delay - self.overhead
-        
-        self.stop_flag = False
-        
-    def start_threaded(self):
-        t = threading.Thread(target=self.start, args=())
-        t.daemon = True
-        t.start()
-        return t
-
-    def calibrate(self, samples):
-        cal_times = []
-        
-        t0 = time.clock()
-        while len(cal_times) < samples:
-            if self.stop_flag:
-                pass   # Don't do anything, but we need the if statement to be consistent with the run method timing
-            t1 = time.clock()
-            if t1 - t0 >= self.delay:
-                cal_times.append(t1 - t0)  # Hopefully this is about the same amount of time as the call, but it doesn't really matter as we've already calculated t1
-                t0 = t1
-        
-        tl_time = 0.0
-        n_times = 0
-        for t in cal_times:
-            #print "{:.20f}, {:.20f}".format(t, self.requested_delay)
-            if t <= (self.requested_delay * 3) or True:  # Skip the outliers
-                tl_time += t
-                n_times += 1
-        
-        if n_times == 0:
-            print "Hmm -- got {} but all were outliers...".format(len(cal_times))
-            exit()
-        
-        avg_time = tl_time / n_times
-        print "Calibration: target is {:.20f}, measured is {:.20f} with {} samples and {} outliers.".format(self.interval_usec, avg_time, samples, len(cal_times) - n_times)
-        adjustor = 25 * (avg_time - self.requested_delay)  # Note: 25 because windows. Not a great thing to have a magic number, but it works
-        old_overhead = self.overhead
-        self.overhead += adjustor  # Adjust overhead
-        print "Adjusting overhead by {:.20f} from {:.20f} to {:.20f}".format(adjustor, old_overhead, self.overhead)
-        self.delay = self.requested_delay - self.overhead  # Recalculate delay
-        print "Delay is now {:.20f} (target {:.20f})".format(self.delay, self.requested_delay)
         
 
-
-    def start(self):
-        t0 = time.clock()
-        while True:
-            if self.stop_flag:  # Break out if we need to
-                self.stop_flag = False  # so we can restart
-                break
-            t1 = time.clock()
-            if t1 - t0 >= self.delay:
-                self.callback()
-                t0 = t1
-                
-    def stop(self):
-        self.stop_flag = True
-        
-
-class TimerUsecTest:
-    def __init__(self, timer_usec):
-        self.timer_usec = timer_usec
-        self.timer_usec.callback = self.callback
-
-        self.times = []
-
-        self.run_t0 = None
-        self.t0 = None
-        self.t1 = None
+class TimeDialator(Timer):
+    """ A timer that controls the dialation of time (sim time vs real time) for other timers.
+        Note that this can either be run as a timer or stepped directly by the simulation (use either, but not both probably.)
+        @todo: Probably split this into two classes: one that can be run as a timer and one that is stepped by the simulator. Stepping will be more accurate. 
+    """
     
-
-    def run(self, seconds):
-        #t0 = datetime.now()
-
-        self.times = []
-        self.run_t0 = time.clock()
-
-        self.timer_usec.callback = self.callback
-        self.timer_usec.run_threaded()
-        
-        print "Running TimerUsecTest with TimerUsec.delay {:.20f}".format(self.timer_usec.delay)
-        
-        # Wait for the appropriate number of seconds
-        while time.clock() - self.run_t0 <= seconds:
-            pass
-        
-        self.timer_usec.stop()
-
-        tl_time = 0.0
-        n_times = 0
-        
-        for t in self.times:
-            if t <= 3 * self.timer_usec.requested_delay:
-                tl_time += t
-                n_times += 1
-        
-        avg_time = (tl_time / n_times)
-        n_outliers = len(self.times) - n_times
-        
-        print "{} calls completed in {} seconds with {} outliers; average of {:.20f} (target {:.20f})".format(n_times, seconds, n_outliers, avg_time, self.timer_usec.requested_delay)
-        
-        
-    def callback(self):
-        if self.t0 is None:
-            self.t0 = time.clock()
-            return 
-            
-        self.t1 = time.clock()
-        self.times.append(self.t1 - self.t0)
-        self.t0 = self.t1
-        
-    
-    
-class TimeSync:
-    def __init__(self, sim):
-        """ Threaded clock synchronizer """
+    def __init__(self, sim, interval_sec=None, timers=None):
+        Timer.__init__(self, interval_sec, self.dialate_time)
         self.sim = sim
+        
+        self.timers = timers or []
 
-        self.last_step_t = 0.0
-        self.last_step_duration = None
+        self.last_real_time = None
+        self.last_sim_time = None
+
+    def add_timer(self, timer):
+        self.timers.append(timer)
     
-    def step_callback(self, sensor, step_samples):
-        """ Gets called during every step. Samples are the elapsed simulator time """
-        new_step_t = time.clock()
-        if self.last_step_duration is None:
-            self.last_step_t = new_step_t
+    def add_timers(self, timers):
+        self.timers.extend(timers)
+    
+    def dialate_time(self, dt_usec=None):
+        """ Calculate the time dialation and set it on our timers """
+        if self.last_real_time is None:
+            # Initialize here so that we can ignore thread startup times and whatnot
+            self.last_real_time = time.clock()
+            self.last_sim_time = self.sim.elapsed_time_usec * 1000000.0
             return
+
+        real_time_diff = time.clock() - self.last_real_time
+        if dt_usec is None:
+            # We didn't have one passed in, so calculate it. This is used if we're running as a timer rather than directly stepped. 
+            sim_time_diff = (self.sim.elapsed_time_usec / 1000000.0) - self.last_sim_time 
+        else:
+            sim_time_diff = dt_usec / 1000000.0
+            
+        # Dialation is the ratio of real time to sim time: 10s real time to 1s sim time = dialation factor of 10. Or 1s real time to 2s sim time = dialation factor of .5
+        self.dialation = real_time_diff / sim_time_diff
+        for timer in self.timers:
+            timer.set_dialation(self.dialation)
+        self.logger.debug("Set time dialation to {}".format(self.dialation))
         
-        self.last_step_duration = time.clock() - self.last_step_t
+        # Update our times
+        self.last_real_time = time.clock()
+        self.last_sim_time = self.sim.elapsed_time_usec / 1000000.0
+
+    def step(self, dt_usec):
+        """ step method so that we can be directly stepped instead of used as a timer """
+        self.dialate_time(dt_usec)
         
-        
-    def run(self):
-        pass
+# @todo: add a TimeAdjustor class that adjust delay (and maybe overhead) for timers that are lagging/speeding
+
 
 class TimerTest:
     # Test a 10 microsecond timer in python on windows
@@ -225,15 +145,6 @@ class TimerTest:
 if __name__ == "__main__":
     print "-- Timer Test -----"
 
-    #tt = TimerTest(100000)
-    #tt.run()
+    tt = TimerTest(100000)
+    tt.run()
 
-    #print tt.results()
-
-    tu = TimerUsec(50, None)
-    tut = TimerUsecTest(tu)
-    tut.run(5)
-
-    tu.calibrate(1000)
-    
-    tut.run(5)
